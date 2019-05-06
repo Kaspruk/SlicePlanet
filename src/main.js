d78585114515454151d7f3f4a3d5dc341c087698
Babiy Oleg <babiyoleg@gmail.com>
2019-05-06 17:59:04 +0300

    Create Project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
+import Backendless from 'backendless';
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false
+const APP_ID = '18D69D2E-15FE-994E-FF52-306D336F9F00';
+const API_KEY = '66F743BC-7A95-0572-FFBD-7EF5289AA100';
+Backendless.initApp(APP_ID, API_KEY);
+Object.defineProperty(Vue.prototype, 'API', { value: Backendless });
+Object.defineProperty(Vue.prototype, 'dataQueryBuilderSort', { value: Backendless.DataQueryBuilder.create().setSortBy(['sort ASC']) });
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
